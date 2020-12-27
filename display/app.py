5c74259dfaa5c4daf231c1dc2f25a30311cf70eb
Forever-CodingNoob <johnnylin6256@gmail.com>
2020-12-27 13:04:40 +0800

    add variables to change

--- display/app.py
+++ display/app.py
@@ -45,6 +45,18 @@ params = {
         max=3,
         primary_step=1,
         secondary_step=0.1
+    ),
+    'I_sym': Param(
+        min=0,
+        max=150,
+        primary_step=10,
+        secondary_step=1
+    ),
+    'R': Param(
+        min=0,
+        max=150,
+        primary_step=10,
+        secondary_step=1
     )
 }
 
@@ -55,55 +67,73 @@ app.layout = html.Div([
     dcc.Graph(id='seirs',
               animate=True
               ),
-    dcc.Slider(
-        id='beta',
-        min=params['beta'].min,
-        max=params['beta'].max,
-        value=params['beta'].mid(),
-        step=params['beta'].secondary_step,
-        marks={(round(val,10) if not val==int(val) else int(val)): {'label': str(round(val,10))} for val in params['beta'].array()}
-    )
+    *[
+        html.Div([
+            dcc.Slider(
+                id=key,
+                min=param.min,
+                max=param.max,
+                value=param.mid(),
+                step=param.secondary_step,
+                marks={(round(val,10) if not val==int(val) else int(val)): {'label': str(round(val,10))} for val in param.pmy_array()}
+            ),
+            html.Div(id=f'{key}-label',style={'margin-bottom':20})
+        ]) for key,param in params.items()
+    ]
+
 ])
 
 
 @app.callback(
     dash.dependencies.Output('seirs', 'figure'),
-    [dash.dependencies.Input('beta', 'value')])
-def update_figure(beta):
+    [dash.dependencies.Input(key, 'value') for key in params.keys()])
+def update_figure(*vals):
     traces = []
-    print('beta:', beta)
-    SEIRS, params = simulateSEIRS(beta=beta,day=30)
-    print(SEIRS, params, sep='\n')
+    print(vals)
+    SEIRS, params_used = simulateSEIRS(**{key: val for key, val in zip(params.keys(), vals)}, day=30)
+    print(SEIRS, params_used, sep='\n')
 
     length = np.size(SEIRS, axis=0)
     types_amount = np.size(SEIRS, axis=1)
-    t = np.arange(0, length, 1) * (params['interval'])  # [0,0+interval,0+interval*2,...,day]，即每筆資料對應之時刻
+    t = np.arange(0, length, 1) * (params_used['interval'])  # [0,0+interval,0+interval*2,...,day]，即每筆資料對應之時刻
 
     # 運用內差法
     # 繪製曲線
     dt = 0.05
-    tnew = np.arange(0, int(params['day'] / dt) + 1, 1) * dt  # [0,0+dt,0+dt*2,...,day]
+    tnew = np.arange(0, int(params_used['day'] / dt) + 1, 1) * dt  # [0,0+dt,0+dt*2,...,day]
     # print(tnew)
 
     traces = [go.Scatter(
         x=tnew,
         y=interpolate.InterpolatedUnivariateSpline(t, SEIRS[:, i])(tnew),
-        text=params['compartments'][i],
         mode='lines',
         opacity=0.7,
-        name=params['compartments'][i]
+        name=params_used['compartments'][i]
     ) for i in range(types_amount)]
 
     return {
         'data': traces,
         'layout': go.Layout(
-            xaxis={'title': 'day', 'range': [0,params['day']]},
-            yaxis={'title': 'population', 'range': [0,np.max(SEIRS[:, -1]) * 1.2]},
+            xaxis={'title': 'day', 'range': [0, params_used['day']]},
+            yaxis={'title': 'population', 'range': [0, np.max(SEIRS[:, -1]) * 1.2]},
             margin={'l': 100, 'b': 40, 't': 10, 'r': 10},
             legend=go.layout.Legend(),
             hovermode='closest'
         )
     }
+
+def gen_updLabel_func(keyname):
+    def upd_label(val):
+        return keyname+" = "+str(val)
+    return upd_label
+for key,param in params.items():
+    # upd_label.__closure__[0].cell_contents = key
+
+    app.callback(
+        dash.dependencies.Output(f'{key}-label', 'children'),
+        [dash.dependencies.Input(key, 'value')]
+    )(gen_updLabel_func(keyname=key))
+
 
 
 if __name__ == '__main__':
